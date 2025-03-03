c5242f5532bbf288ce2327397c4a5a20e5c4864c
GloriaAtieno <atienogloria07@gmail.com>
2025-03-03 15:10:34 +0300

    Sports Management System

---
+++ Controllers/AssociationManager/AlreadyPlayedMatchesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sports_Management_System.Models;
+
+namespace Sports_Management_System.Controllers.AssociationManager
+{
+	[Authorize(Roles = "AssociationManager")]
+	[Route("api/already-played-matches")]
+    [ApiController]
+    public class AlreadyPlayedMatchesController : Controller
+    {
+        private readonly ChampionsLeagueDbContext _db;
+        public AlreadyPlayedMatchesController(ChampionsLeagueDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetMatchesAsync()
+        {
+            return Json(new { data = await _db.Set<AlreadyPlayedMatch>().ToListAsync() });
+        }
+    }
+}

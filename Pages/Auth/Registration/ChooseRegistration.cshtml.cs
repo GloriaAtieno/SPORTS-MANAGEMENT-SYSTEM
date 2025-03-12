130afce9fa0be2234d2d83f16ff11eeae299b9ba
GloriaAtieno <atienogloria07@gmail.com>
2025-03-12 12:03:43 +0300

    Sports Management System

---
+++ Pages/Auth/Registration/ChooseRegistration.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sports_Management_System.Pages.Auth
+{
+    public class ChooseRegistrationModel : PageModel
+    {
+    }
+}

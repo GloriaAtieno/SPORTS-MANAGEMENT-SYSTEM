4f654c4b4d7718aa4c37e4a67648873e47d84a1e
GloriaAtieno <atienogloria07@gmail.com>
2025-03-12 12:03:59 +0300

    Sports Management System

---
+++ Models/Club.cs
@@ -0,0 +1,16 @@
+﻿namespace Sports_Management_System.Models;
+
+public partial class Club
+{
+    public int ClubId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Location { get; set; }
+
+    public virtual ICollection<ClubRepresentative> ClubRepresentatives { get; } = new List<ClubRepresentative>();
+
+    public virtual ICollection<Match> MatchGuestClubs { get; } = new List<Match>();
+
+    public virtual ICollection<Match> MatchHostClubs { get; } = new List<Match>();
+}

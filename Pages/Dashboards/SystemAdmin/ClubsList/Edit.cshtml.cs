c559f61e0063077d9e466ee5e741db28194f2bc0
GloriaAtieno <atienogloria07@gmail.com>
2025-03-13 11:20:48 +0300

    Sports Management System

---
+++ Pages/Dashboards/SystemAdmin/ClubsList/Edit.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sports_Management_System.Models;
+
+namespace Sports_Management_System.Pages.Dashboards.SystemAdmin.ClubsList
+{
+	[Authorize(Roles = "SystemAdmin")]
+	public class EditModel : PageModel
+    {
+        private readonly ChampionsLeagueDbContext _db;
+        public string ErrorMessage = "";
+        public EditModel(ChampionsLeagueDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Club Club { get; set; }
+        public async Task OnGet(int id)
+        {
+            Club = await _db.Clubs.FindAsync(id);
+        }
+        private async Task<string> GetErrorMessage()
+        {
+            if (!ModelState.IsValid)
+            {
+                return "Fill All Fields Correctly";
+            }
+            return "";
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            ErrorMessage = await GetErrorMessage();
+            if(ErrorMessage != "")
+            {
+                return Page();
+            }
+            var ClubFromDb = await _db.Clubs.FindAsync(Club.ClubId);
+            ClubFromDb.Name = Club.Name;
+            ClubFromDb.Location = Club.Location;
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}

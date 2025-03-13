d00435ae630e4207bb4503a38c2af7fb6f38b2ab
GloriaAtieno <atienogloria07@gmail.com>
2025-03-13 11:17:38 +0300

    Sports Management System

---
+++ wwwroot/js/associationManager/clubsNeverMatched.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('#ClubsTable').DataTable({
+        "ajax": {
+            "url": "/api/clubs-never-matched",
+            "type": 'GET',
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "club1Name", "width": "20%" },
+            { "data": "club2Name", "width": "20%" }
+        ],
+        "language": {
+            "emptyTable": "Every Club is Matched Until Now"
+        },
+        "width": "100%"
+    });
+});

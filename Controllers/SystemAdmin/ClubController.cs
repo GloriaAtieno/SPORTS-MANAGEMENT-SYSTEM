91a11592d493c86eea4fec39827c23a088e7e40f
GloriaAtieno <atienogloria07@gmail.com>
2025-03-13 11:15:09 +0300

    Sports Management System

---
+++ Controllers/SystemAdmin/ClubController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sports_Management_System.Models;
+
+namespace Sports_Management_System.Controllers.SystemAdmin
+{
+    [Authorize(Roles = "SystemAdmin")]
+    [Route("api/clubs")]
+    [ApiController]
+    public class ClubController : Controller
+    {
+        private readonly ChampionsLeagueDbContext _db;
+        public ClubController(ChampionsLeagueDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetClubs()
+        {
+            return Json(new { data = await _db.Clubs.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int clubId)
+        {
+            var clubFromDb = await _db.Clubs.FirstOrDefaultAsync(u => u.ClubId == clubId);
+            if (clubFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            var ClubReprsentative = await _db.GetClubRepresentativeAsUser(clubFromDb.ClubId);
+            if (ClubReprsentative != null)
+            {
+                _db.SystemUsers.Remove(ClubReprsentative);
+            }
+            _db.Clubs.Remove(clubFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete is Successful" });
+        }
+    }
+}

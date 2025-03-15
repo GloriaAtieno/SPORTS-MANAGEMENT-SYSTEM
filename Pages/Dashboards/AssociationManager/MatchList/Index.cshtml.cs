4051aa86ddd67fa3d4d163e9372d9863b6c3ba68
GloriaAtieno <atienogloria07@gmail.com>
2025-03-15 11:06:28 +0300

    Sports Management System

---
+++ Pages/Dashboards/AssociationManager/MatchList/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sports_Management_System.Pages.Dashboards.AssociationManager.MatchList
+{
+	[Authorize(Roles = "AssociationManager")]
+	public class IndexModel : PageModel
+    {
+    }
+}

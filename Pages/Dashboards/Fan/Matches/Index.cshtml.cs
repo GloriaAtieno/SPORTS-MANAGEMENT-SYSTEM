b9f4942827d98b65c43fd9e597b9304fbb0c3af6
GloriaAtieno <atienogloria07@gmail.com>
2025-03-15 11:07:59 +0300

    Sports Management System

---
+++ Pages/Dashboards/Fan/Matches/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sports_Management_System.Models;
+
+namespace Sports_Management_System.Pages.Dashboards.Fan.Matches
+{
+    [Authorize(Roles = "Fan")]
+    public class IndexModel : PageModel
+    {
+    }
+}

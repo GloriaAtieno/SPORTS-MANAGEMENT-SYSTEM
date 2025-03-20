189f5a2ee503f3a5a88af6e092f57a40443320ae
GloriaAtieno <atienogloria07@gmail.com>
2025-03-20 11:43:46 +0300

    Sports Management System

---
+++ Controllers/AssociationManager/UpcomingMatchesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sports_Management_System.Models;
+
+namespace Sports_Management_System.Controllers.AssociationManager
+{
+	[Authorize(Roles = "AssociationManager")]
+	[Route("api/upcoming-matches")]
+    [ApiController]
+    public class UpcomingMatchesController : Controller
+    {
+        private readonly ChampionsLeagueDbContext _db;
+        public UpcomingMatchesController(ChampionsLeagueDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetMatchesAsync()
+        {
+            return Json(new {data = await _db.Set<UpComingMatch>().ToListAsync()});
+        }
+    }
+
+   
+}

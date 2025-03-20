c8dd1437491dd399e6597443a590e0e6e2ccf8d2
GloriaAtieno <atienogloria07@gmail.com>
2025-03-20 11:44:12 +0300

    Sports Management System

---
+++ Controllers/ClubRepresentative/UpcomingMatchesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sports_Management_System.Models;
+using Sports_Management_System.Pages.Auth;
+
+namespace Sports_Management_System.Controllers.ClubRepresentative
+{
+    [Authorize(Roles = "ClubRepresentative")]
+	[Route("api/club-upcoming-matches")]
+    [ApiController]
+    public class UpcomingMatchesController : Controller
+    {
+        private readonly ChampionsLeagueDbContext _db;
+        private string Username;
+
+        public UpcomingMatchesController(ChampionsLeagueDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetMatches()
+        {
+            Username = Auth.GetCurrentUserName(User);
+			Club club = await _db.Clubs.FindAsync((await _db.GetCurrentClubRepresentative(Username)!).ClubId);
+            return Json(new { data = await _db.GetAllUpComingMatchesOfClub(club!.ClubId).ToListAsync()});
+        }
+    }
+}
